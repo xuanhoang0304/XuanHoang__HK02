974f413adcce67b53c6806823087f7e16e5ea997
xuanhoang0304 <ppxxhh111@gmail.com>
2024-05-21 00:15:55 +0700

    add event click menu mobie

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,3 +4,16 @@ const header__textPC = document.querySelector(".header__text");
 const header__textMB = document.querySelector("#toggle-menu__middle");
 header__textMB.innerHTML = header__textPC.innerHTML;
 listmb.innerHTML = listpc.innerHTML;
+
+const Menu = document.querySelector(".mbHeader__menu")
+const toggleMenu = document.querySelector(".toggle-menu");
+const overlay = document.querySelector(".overlay");
+Menu.addEventListener("click", function() {
+    toggleMenu.classList.toggle("click");
+    overlay.classList.toggle("click");
+})
+
+overlay.addEventListener("click", function() {
+    overlay.classList.toggle("click")
+    toggleMenu.classList.toggle("click")
+})
